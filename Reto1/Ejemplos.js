7904c9da2b50f5bf42c1357d5ec73fc46c3eb0a6
Piero Montalvo <piero.montalvo9922@gmail.com>
2020-06-27 09:54:36 -0500

    Reto1

---
+++ Reto1/Ejemplos.js
@@ -0,0 +1,30 @@
+// CAllBACKS
+
+// function miNombre(callbacks){
+//     callbacks('Mi nombre es Piero');
+// }
+
+// miNombre((resultado) => {
+//     console.log(resultado)
+// });
+
+
+
+// PROMISE
+
+// const contraseña = new Promise((resolve,reject) => {
+//     let contra = true;
+//     contra ? resolve('Contraseña correcta') : reject('Contraseña Incorrecta')
+// });
+
+// contraseña.then(() => {
+//     console.log('Contraseña Aceptada')
+// }).catch((e) => {
+//     e = new Error
+//     console.log('Contraseña Incorrecta',e)
+// })
+
+
+
+// CALLBACK HELL
+
